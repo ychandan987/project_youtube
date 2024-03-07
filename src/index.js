67cb79e0bcbd9c336ae5b4f306dd194cd4aa1b05
ychandan987 <chandanyadav.nmims@gmail.com>
2024-03-07 07:57:06 +0530

    connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+
+dotenv.config({
+    path : './env'
+});
 
 connectDB();
 
