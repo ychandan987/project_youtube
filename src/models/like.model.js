2f6fa76bc00a9bd36c01e9da30ebd9333b567b7c
ychandan987 <chandanyadav.nmims@gmail.com>
2024-03-30 16:04:49 +0530

    All models updated

---
+++ src/models/like.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const likeSchema = new Schema(
+    {
+        comment : {
+            type : Schema.Types.ObjectId,
+            ref : "Comment"
+        },
+
+        video : {
+            type : Schema.Types.ObjectId,
+            ref : "Video"
+        },
+
+        likedBy : {
+            type : Schema.Types.ObjectId,
+            ref : "User"
+        },
+
+        tweet : {
+            type : Schema.Types.ObjectId,
+            ref : "Tweet"
+        }
+    },
+    {timestamps : true});
+
+export const Like = mongoose.model("Like", likeSchema);
